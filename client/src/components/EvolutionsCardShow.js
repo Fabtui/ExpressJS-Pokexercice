a170e464f992cb84061db34c68bb9293daf6b407
Fabtui <fabrice.tui@gmail.com>
2023-02-16 16:56:27 +0100

    add pictures on evo

--- client/src/components/EvolutionsCardShow.js
+++ client/src/components/EvolutionsCardShow.js
@@ -4,47 +4,69 @@ export class EvolutionsCarShow extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      ids: [],
       pokemons: []
     }
+  }
+
+  componentWillMount() {
+    this.showEvolve(this.props.evolutions.chain)
   }
 
   showEvolve(chain) {
     let data = []
     if (chain.species) {
-      const url = chain.species.url.replace('https://pokeapi.co/api/v2/pokemon-species/', '/pokemon');
-      data.push({name: chain.species.name, url: url})
+      const id = chain.species.url.replace('https://pokeapi.co/api/v2/pokemon-species/', '').replace('/', '');
+      data.push(parseInt(id))
     }
     if (chain.evolves_to[0]) {
-      const url = chain.evolves_to[0].species.url.replace('https://pokeapi.co/api/v2/pokemon-species', '/pokemon');
-      data.push({name: chain.evolves_to[0].species.name, url: url})
+      const id = chain.evolves_to[0].species.url.replace('https://pokeapi.co/api/v2/pokemon-species/', '').replace('/', '');
+      data.push(parseInt(id))
 
       if (chain.evolves_to[0].evolves_to[0]) {
-        const url = chain.evolves_to[0].evolves_to[0].species.url.replace('https://pokeapi.co/api/v2/pokemon-species', '/pokemon');
-        data.push({name: chain.evolves_to[0].evolves_to[0].species.name, url: url})
+        const id = chain.evolves_to[0].evolves_to[0].species.url.replace('https://pokeapi.co/api/v2/pokemon-species/', '').replace('/', '');
+        data.push(parseInt(id))
 
         if (chain.evolves_to[0].evolves_to[0].evolves_to[0]) {
-          const url = chain.evolves_to[0].evolves_to[0].evolves_to[0].species.url.replace('https://pokeapi.co/api/v2/pokemon-species', '/pokemon');
-          data.push({name: chain.evolves_to[0].evolves_to[0].evolves_to[0].species.name, url: url})
+          const id = chain.evolves_to[0].evolves_to[0].evolves_to[0].species.url.replace('https://pokeapi.co/api/v2/pokemon-species/', '').replace('/', '');
+          data.push(parseInt(id))
         }
       }
-    } 
-    return data
+    }
+
+    this.setState({
+      ids : data
+    })
+    data.forEach(id => {
+      fetch(`http://localhost:5000/api/pokemons/${id}`)
+        .then(res => res.json())
+        .then(data =>{ 
+          this.setState({
+            pokemons: [...this.state.pokemons, data.data]
+          })
+      });
+    })
   }
 
   render() {
-    const evolutions = this.props.evolutions
-    const evolutionPokemons = this.showEvolve(evolutions.chain)
-    console.log(evolutionPokemons);
+    const pokemons = this.state.pokemons
     return (
             <div className="evolutions-card-show" >
             <h4>Evolutions: </h4>
-            <ul>
-              {evolutionPokemons.map(evo => 
-              <li>
-                <a href={evo.url}>{evo.name}</a>
-              </li>
-              )}
-            </ul>
+
+            {( pokemons.length != this.state.ids.length ) ? (
+              <p>Loading...</p> 
+            ): (
+              <ul>
+                {pokemons.map(evo => 
+                <li key={evo.id}>
+                  <a>{evo.name}</a>
+                  <img src={evo.picture}/>
+                </li>
+                )}
+              </ul>
+            )}
+
             </div>
     );
   }
